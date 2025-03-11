163f2c1180c34dc520079d04f2c6414254df2828
vinodchris <vinod09.dx@gmail.com>
2025-03-11 17:16:29 +0530

    vk changes v1

--- src/pages/HealthView/index.js
+++ src/pages/HealthView/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Health from '../../components/HealthViewPage/Health/index'
+import Health from '../../components/HealthViewPage/Health/index';
+import HealthcareData from "../../components/HealthViewPage/Benefits/index";
 const index = () => {
   return (
     <div>
     <Health/>
+    <HealthcareData/>
     </div>
   )
 }
