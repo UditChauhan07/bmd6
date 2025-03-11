24b00ff26aafdabc5f74c9197312855a82c6d36e
NirajDX <niraj.t@designersx.com>
2025-03-11 17:55:05 +0530

    [AP] 24/7 Page Ui implement  (AP v1.1.4)

--- src/pages/HealthView/index.js
+++ src/pages/HealthView/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Health from '../../components/HealthViewPage/Health/index';
-
 import Slider1 from "../../components/HealthViewPage/Slider1/index"
 import HealthcareData from "../../components/HealthViewPage/Benefits/index";
 import Order from '../../components/Order/index'
+import Footer from '../../components/footer/index'
 const index = () => {
   return (
     <div>
@@ -11,6 +11,7 @@ const index = () => {
     <Slider1/>
     <HealthcareData/>
     <Order/>
+    <Footer/>
     </div>
   )
 }
