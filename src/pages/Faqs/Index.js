fa49d6d22b52ef30fe0e95cbed59ec46b9e059d1
NirajDX <niraj.t@designersx.com>
2025-03-12 18:20:43 +0530

    [AP] Faqs page implement Ui (AP v1.1.5)

--- src/pages/Faqs/Index.js
+++ src/pages/Faqs/Index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Frequently from '../../components/FaqsPage/Frequently'
 import  FaqsTabing from '../../components/FaqsPage/FaqsTabing'
+import Footer from '../../components/footer/index'
 
 const Index = () => {
   return (
     <div>
       <Frequently />
       <FaqsTabing/>
+      <Footer />
     </div>
   )
 }
