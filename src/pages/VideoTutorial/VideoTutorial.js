e027decf49ab2b42ad0a5d93cbf6bf1e25bcc2b4
NirajDX <niraj.t@designersx.com>
2025-03-19 14:19:15 +0530

    [AP] Home Changes (AP v1.9)

--- src/pages/VideoTutorial/VideoTutorial.js
+++ src/pages/VideoTutorial/VideoTutorial.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Study from '../../components/VideoTutorialPage/Studies/index'
 import VideoStudy from '../../components/VideoTutorialPage/VideoStudy/index'
-import Footer from '../../components/footer'
 
+import NewFooter from '../../components/NewFooter/Index'
 const VideoTutorial = () => {
   return (
     <div>
       <Study/>
       <VideoStudy/>
-      <Footer/>
+      <NewFooter/>
 
     </div>
   )
